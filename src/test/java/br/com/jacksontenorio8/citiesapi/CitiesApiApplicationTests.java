cdd9cdce03dd9e1a8266f95bc794115b37d8fa00
jacksontenorio8 <jacksontenorio8@gmail.com>
2021-10-19 16:16:27 -0300

    Crie o uma api rest

---
+++ src/test/java/br/com/jacksontenorio8/citiesapi/CitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jacksontenorio8.citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
